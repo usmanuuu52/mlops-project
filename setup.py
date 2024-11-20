1b529df66763c870aaeb1832be6a11852754d2f7
usman <usmankhanuuu52@gmail.com>
2024-11-20 12:42:24 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A mini project implementing mlops concepts in a data science project',
+    author='usman nasir',
+    license='',
+)
